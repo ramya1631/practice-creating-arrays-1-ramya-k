a682a710ffc381e9f266cc15d57fa421e3772c12
ramya <ramya.koluguri16@gmail.com>
2025-03-19 19:25:39 -0500

    Creating arrays

---
+++ practiceCreatingArrays.js
@@ -0,0 +1,22 @@
+//Task 1 :creating arrays
+let groceryStoreItems =["banana","bread","eggs","chocolates","milk"];
+let prices=[0.44,4.78,6.90];
+
+
+//Task 2:Access and log the values
+console.log(" The array of Grocery items:",groceryStoreItems);
+console.log("The array of Grocery prices:", prices);
+
+
+console.log("Individual GroceryStoreItems:");
+console.log(groceryStoreItems[1]); // Log second item
+console.log(groceryStoreItems[3]); // Log fourth item
+console.log(groceryStoreItems[0]); // Log first item
+
+console.log("Selected Prices:");
+console.log(prices[1]); // Log second price
+console.log(prices[0]); // Log first price
+
+
+
+
